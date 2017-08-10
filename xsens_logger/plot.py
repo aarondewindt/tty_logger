9c9979a9fc9f4999ca6dcb867159ae7100ba0b6d
Aaron M. de Windt <aaron.dewindt@gmail.com>
2017-08-10 21:45:07 +0200

    plot file now uses the File class from sim_common to open files. Pushing calibration data which I had somehow missed.

--- xsens_logger/plot.py
+++ xsens_logger/plot.py
@@ -1,24 +1,23 @@
 import matplotlib.pyplot as plt
-import pickle
+from sim_common import File
 import numpy as np
 
 
-with open("xsens_log_18.pickle", "rb") as f:
-    tables = pickle.load(f)
+tables = File("../xsens_log_0.msgp.gz").load()
 
-    tables['packet_counter_diff'] = {'time': None, 'data': None}
-    tables['packet_counter_diff']['time'] = tables['packet_counter']['time'][:-1]
-    tables['packet_counter_diff']['data'] = np.diff(tables['packet_counter']['data'])
+tables['packet_counter_diff'] = {'time': None, 'data': None}
+tables['packet_counter_diff']['time'] = tables['packet_counter']['time'][:-1]
+tables['packet_counter_diff']['data'] = np.diff(tables['packet_counter']['data'])
 
-    tables['sampling_frequency'] = {'time': None, 'data': None}
-    tables['sampling_frequency']['time'] = tables['packet_counter']['time'][:-1]
-    tables['sampling_frequency']['data'] = 1./np.diff(tables['packet_counter']['time'])
+tables['sampling_frequency'] = {'time': None, 'data': None}
+tables['sampling_frequency']['time'] = tables['packet_counter']['time'][:-1]
+tables['sampling_frequency']['data'] = 1./np.diff(tables['packet_counter']['time'])
 
-    for key, content in tables.items():
-        print(key)
-        if isinstance(content, dict):
-            fig = plt.figure()
-            fig.suptitle(key)
-            plt.plot(content['time'], content['data'])
+for key, content in tables.items():
+    print(key)
+    if isinstance(content, dict):
+        fig = plt.figure()
+        fig.suptitle(key)
+        plt.plot(content['time'], content['data'])
 
-    plt.show()
+plt.show()
